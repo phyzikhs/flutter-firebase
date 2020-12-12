f775788a22a367527c4291c5c89caaeb9a4281a7
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-13 00:11:14 +0200

    wapper on build readsuser changes stream

--- brew_crew/lib/main.dart
+++ brew_crew/lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:brew_crew/models/user_bc.dart';
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import 'package:brew_crew/screens/home/home.dart';
 import 'package:brew_crew/screens/wrapper.dart';
-import 'package:flutter/material.dart';
+import 'package:brew_crew/services/auth.dart';
 
+import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,8 +18,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Authenticate(),
+    return StreamProvider<UserBC>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Authenticate(),
+      ),
     );
   }
 }
