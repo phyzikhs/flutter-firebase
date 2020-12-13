0242beb2c77a3dd5feb2e104da48b39748da432f
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-13 07:22:34 +0200

    dropdown from setting basically popup menu in home

--- brew_crew/lib/screens/home/home.dart
+++ brew_crew/lib/screens/home/home.dart
@@ -10,6 +10,17 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void _showSettingsPannel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+              child: Text('bottom sheet'),
+            );
+          });
+    }
+
     return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
@@ -25,6 +36,11 @@ class Home extends StatelessWidget {
                 await _auth.signOut();
               },
               label: Text('Log out'),
+            ),
+            FlatButton.icon(
+              icon: Icon(Icons.settings),
+              onPressed: () => _showSettingsPannel(),
+              label: Text('Settings'),
             ),
           ],
         ),
